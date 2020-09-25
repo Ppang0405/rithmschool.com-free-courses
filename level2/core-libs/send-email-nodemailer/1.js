6f9c9edb6b0d13e05ec72c34797dd0b3be94901c
Truong Nguyen <nguyentruong.xyz0405@gmail.com>
2020-09-25 23:56:01 +0700

    add send mail node and todo things

---
+++ level2/core-libs/send-email-nodemailer/1.js
@@ -0,0 +1,24 @@
+/*
+
+
+Objectives:
+
+By the end of this chapter, you should be able to:
+
+    Define what a mailer is
+    Use nodemailer to send users emails
+
+ */
+
+/*
+
+
+mailers
+
+Another very common task when building backend applications is sending email to users. This requires setting up an email server and configuring it with your transactional mail provider (Mandrill, SendGrid, Gmail etc.). To get started sending mail to your users, check out Nodemailer. 
+ */
+
+
+// send email in node use mailgun free account and modemailer lib
+// 
+// // -> exercise please do something good
---
+++ todo.md
@@ -0,0 +1,6 @@
+# Todo:
+
+- send mail in nodejs: mailers
+- background job -> use redis
+- socket with private rooms
+- web scraper -> giá bỉm trung bình trên shoppe
