051368a1197a709e41ae25d267bee6c5a78684e9
Truong Nguyen <nguyentruong.xyz0405@gmail.com>
2020-09-25 13:23:37 +0700

    add some folder

---
+++ fundamentals/node-sql-express/intro-postgres-with-node/1.js
@@ -0,0 +1,11 @@
+/**
+ *
+ *
+ *
+ * By the end of this chapter, you should be able to:
+
+    Introduce the pg module and how to make queries
+    Describe the landscape of adapters, query builders and ORMs for postgres and node
+    Execute simple SQL statements using the pg module
+
+ */
