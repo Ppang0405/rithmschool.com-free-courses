53296c89a3f6a517dd16ccaf40ce829fff584e95
Truong Nguyen <nguyentruong.xyz0405@gmail.com>
2020-09-25 12:50:40 +0700

    re-structure code 🏀

---
+++ fundamentals/intro-express/intro-express-2/2.js
@@ -0,0 +1,21 @@
+/**
+ *
+ *
+ * Objectives:
+
+By the end of this chapter, you should be able to:
+
+    Move routing logic into a separate file using the express router
+    Use the express router to refactor routing code
+
+
+
+
+1.
+
+Router
+
+So far we have seen how to create routes in express and render templates and even respond with redirects! As we start building more and more routes, our app.js can get very messy quickly.
+
+In order to clean this up, we will move our routes to a file in a folder called routes. The files in this folder will contain all of our routes and we will export them using the express router. Let's get started with a simple application.
+ */
--- fundamentals/intro-express/intro-express-2/app.js
+++ fundamentals/intro-express/intro-express-2/app.js
@@ -7,21 +7,21 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get("/", (req, res) => {
-  res.status(200).json({ name: "Elie" });
+    res.status(200).json({ name: "Elie" });
 });
 
 app.get("/instructor/:firstName", (req, res) => {
-  return res.send(
-    `The name of this instructor is ${req.params.firstName}`
-  );
+    return res.send(
+        `The name of this instructor is ${req.params.firstName}`
+    );
 });
 
 app.get("/secret", (req, res) => {
-  res.status(401).json({ message: "Unauthorized" });
+    res.status(401).json({ message: "Unauthorized" });
 });
 
 app.listen(3000, () => {
-  console.log(
-    "The server has started on port 3000. Head to localhost:3000 in the browser and see what's there!"
-  );
+    console.log(
+        "The server has started on port 3000. Head to localhost:3000 in the browser and see what's there!"
+    );
 });
