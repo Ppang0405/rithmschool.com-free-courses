81df29a48939983626dc5814408153686542d4f3
Truong Nguyen <nguyentruong.xyz0405@gmail.com>
2020-11-16 13:12:32 +0700

    add intro exercise

---
+++ python/level1/intro/bool_logic.py
@@ -0,0 +1,11 @@
+"""
+bjectives
+
+By the end of this chapter, you should be able to:
+
+    Write conditional logic using boolean operators
+    List examples of falsey values in Python
+    Use if/else to include conditional logic in your Python code
+    Explain the difference between == and is in Python
+    Convert between data types explicitly in Python
+"""
